19a462a0dddcfe598f2835f7ca1c62321155dc04
cjj <106533830@qq.com>
2018-06-30 18:37:03 +0800

    Basic user profile editing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
-    //个人展示
+    //个人展示中心
     public function show(User $user)
     {
     	return view('users.show',compact('user'));
+    }
+    //个人编辑中心
+    public function edit(User $user)
+    {
+    	return view('users.edit',compact('user'));
+    }
+    //更新个人资料
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
