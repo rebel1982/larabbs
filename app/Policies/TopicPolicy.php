3bd4d96d6f9e93faa5da779a2edc076445a23f6d
cjj <106533830@qq.com>
2018-07-06 07:24:57 +0800

    Author can delete topic

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -9,11 +9,15 @@ class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
     {
-        return $topic->user_id == $user->id;
+//        return $topic->user_id == $user->id;
+        //代码复用
+        //在User模型中定义isAuthorOf方法
+        return $user->isAuthorOf($topic);
     }
 
     public function destroy(User $user, Topic $topic)
     {
-        return true;
+//        return $topic->user_id == $user->id;
+        return $user->isAuthorOf($topic);
     }
 }
