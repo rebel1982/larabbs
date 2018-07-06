9467b325b3a5dbefaf3ec2877cf10f66a50a59c9
cjj <106533830@qq.com>
2018-07-06 22:11:17 +0800

    Auto translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-
+use App\Handlers\SlugTranslateHandler;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -27,7 +27,11 @@ class TopicObserver
     {
         //过滤数据
         $topic->body = clean($topic->body, 'user_topic_body');
-        //SEO
+        //生成话题摘录，调用自定义函数make_excerpt
         $topic->excerpt = make_excerpt($topic->body);
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
