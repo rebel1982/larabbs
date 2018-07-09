63abbb2f137fc1b2c3c152c7477095da66557487
cjj <106533830@qq.com>
2018-07-10 06:49:36 +0800

    Sidebar active users

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use Auth;
 use App\Handlers\ImageUploadHandler;
+use App\Models\User;
 
 class TopicsController extends Controller
 {
@@ -17,13 +18,15 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request , Topic $topic)
+	public function index(Request $request , Topic $topic ,User $user)
 	{
 		//$topics = Topic::paginate(30);
 		// Eloquent 提供的 预加载功能 
 		// $topics = Topic::with('user', 'category')->paginate(15);
 		$topics = $topic->withOrder($request->order)->paginate(20);
-		return view('topics.index', compact('topics'));
+        $active_users = $user->getActiveUsers();
+//        dd($active_users);
+		return view('topics.index', compact('topics','active_users'));
 	}
 
     public function show(Request $request,Topic $topic)
